5b3ca856bde7d60330f25eb03ee6b10b31e8f23c
Mike Sun <mike@msun.io>
2023-12-06 20:10:45 -0500

    Day 6.

---
+++ day06.zig
@@ -0,0 +1,80 @@
+const std = @import("std");
+
+const input = "day06.input";
+var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+
+fn partOne() !void {
+    var file = try std.fs.cwd().openFile(input, .{});
+    defer file.close();
+    var reader = file.reader();
+
+    var doc = [2]std.ArrayList(usize){ undefined, undefined };
+    for (0..2) |i| {
+        var line = std.ArrayList(u8).init(gpa.allocator());
+        try reader.streamUntilDelimiter(line.writer(), '\n', null);
+
+        var it = std.mem.splitAny(u8, try line.toOwnedSlice(), ":");
+        _ = it.first();
+
+        var nums_it = std.mem.splitAny(u8, std.mem.trim(u8, it.rest(), " "), " ");
+        doc[i] = std.ArrayList(usize).init(gpa.allocator());
+        while (nums_it.next()) |n| {
+            var num = std.fmt.parseInt(usize, std.mem.trim(u8, n, " "), 10) catch continue;
+            try doc[i].append(num);
+        }
+    }
+
+    const times = try doc[0].toOwnedSlice();
+    const records = try doc[1].toOwnedSlice();
+    var product: usize = 1;
+
+    for (times, records) |t, r| {
+        var ways: usize = 0;
+        for (0..t + 1) |speed| {
+            const d = distance(t, speed);
+            if (d > r) ways += 1;
+        }
+        product *= ways;
+    }
+    std.debug.print("part_one product={}\n", .{product});
+}
+
+fn distance(time: usize, speed: usize) usize {
+    return (time - speed) * speed;
+}
+
+fn partTwo() !void {
+    var file = try std.fs.cwd().openFile(input, .{});
+    defer file.close();
+    var reader = file.reader();
+
+    var doc = [2]std.ArrayList(u8){ undefined, undefined };
+    for (0..2) |i| {
+        var line = std.ArrayList(u8).init(gpa.allocator());
+        try reader.streamUntilDelimiter(line.writer(), '\n', null);
+
+        var it = std.mem.splitAny(u8, try line.toOwnedSlice(), ":");
+        _ = it.first();
+
+        var nums_it = std.mem.splitAny(u8, std.mem.trim(u8, it.rest(), " "), " ");
+        doc[i] = std.ArrayList(u8).init(gpa.allocator());
+        while (nums_it.next()) |n| {
+            const num = std.mem.trim(u8, n, " ");
+            if (num.len > 0) try doc[i].appendSlice(num);
+        }
+    }
+
+    const time = try std.fmt.parseInt(usize, try doc[0].toOwnedSlice(), 10);
+    const record = try std.fmt.parseInt(usize, try doc[1].toOwnedSlice(), 10);
+    var ways: usize = 0;
+    for (0..time + 1) |speed| {
+        const d = distance(time, speed);
+        if (d > record) ways += 1;
+    }
+    std.debug.print("part_two ways={}\n", .{ways});
+}
+
+pub fn main() !void {
+    try partOne();
+    try partTwo();
+}
